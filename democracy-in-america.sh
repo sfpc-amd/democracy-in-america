52467ad8f976bbe2d3acfe6f3e921e357abc7461
Andy Dayton <amdayton@gmail.com>
2015-09-29 17:52:38 -0400

    Update shell script

--- democracy-in-america.sh
+++ democracy-in-america.sh
@@ -1,27 +1,28 @@
 # DEMOCRACY IN AMERICA
 
-# download Alexis de Tocqueville's "Democracy in America"
-# from Project Giutenberg, remove front/end matter
-# and save as text file
-curl http://www.gutenberg.org/cache/epub/13333/pg13333.txt | tail -9000 | head -7500 > democracy_in_america.txt
+# create a temp file for the source
+TMPFILE=`mktemp -t democracy_in_america.XXXXXX`
+
+# these are our string searches
+declare -a SEARCHES=("free" "slave" "uncertain")
+
+# download Alexis de Tocqueville's "Democracy in America" from Project Giutenberg,
+# remove front/end matter and save in a temp file
+curl http://www.gutenberg.org/cache/epub/13333/pg13333.txt | tail -9000 | head -7500 > $TMPFILE
+
+# cat democracy_in_america.src.txt | tail -9000 | head -7500 > $TMPFILE
 
 # print the title to an output text file
-printf "\n\nDEMOCRACY IN AMERICA\n\n" > output.txt
+printf "\n\nDEMOCRACY IN AMERICA\n\n" > democracy_in_america.txt
 
-# perform the same operation for each of these words
-for s in "free slave uncertain"
-do
-	# search for the word and perform some editing / manipulation
-	cat < democracy_in_america.txt | grep -i $s | cut -d ' ' -f 2-5 | tr A-Z a-z | tr -d "',." >> output.txt
-	# add a blank line
-	printf "\n" >> output.txt
+
+# perform each string search and output to text file
+for search in "${SEARCHES[@]}"
+do 
+	# run the same search for each word and do some simple string manipulation
+	cat < $TMPFILE | grep -e $search | cut -d ' ' -f 2-5 | tr A-Z a-z | tr -d "',." >> democracy_in_america.txt
+	printf "\n" >> democracy_in_america.txt
 done
 
-# remove the source text
-rm democracy_in_america.txt
-
-# replace with the output text
-mv output.txt democracy_in_america.txt
-
-#show the contents
+# show the contents
 cat democracy_in_america.txt
