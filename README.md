e28c20dae7340a7cf7aa79fd24dc929ebd3931b3
Andy Dayton <amdayton@gmail.com>
2015-09-30 17:15:17 -0400

    Add new txt files and updated script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Democracy in America
 ====================
 
-This bash poem (?) will download the entire text source of Alexis de Tocqueville's [_Democracy in America_](https://en.wikipedia.org/wiki/Democracy_in_America) from Project Gutenberg and use some simple string searches to compose a poem from its contents.
+This bash poem (?) will generate poems from the text of Alexis de Tocqueville's [_Democracy in America_](https://en.wikipedia.org/wiki/Democracy_in_America) (downloaded from Project Gutenberg). You will be prompted for a search term, which will be used to generate the poem.
 
-**Note:** Project Gutenberg applies a limit to the number of books you are able to download in a single day. If you run this script multiple times you may be locked out for 24 hours.
+
--- bin/democracy_in_america.sh
+++ bin/democracy_in_america.sh
@@ -1,60 +1,7 @@
 # DEMOCRACY IN AMERICA
 
-# create a temp file for the source
-# TMPFILE=`mktemp -t democracy_in_america.XXXXXX`
-# TMPFILE=`mktemp -t democracy_in_america`
-TMPFILE="democracy_in_america.src.txt"
+SRC="src/democracy_in_america.txt"
 read SEARCH
-# OUT=''
 
-# these are our string searches
-# declare -a SEARCHES=("free" "slave" "uncertain")
-# declare -a SEARCHES=("free" "slave")
-
-
-# download Alexis de Tocqueville's "Democracy in America" from Project Giutenberg,
-# remove front/end matter and save in a temp file
-# cat democracy_in_america.src.txt | tail -9000 | head -7500 > $TMPFILE
-
-if [ ! -f "$TMPFILE" ]; then
-   # curl http://www.gutenberg.org/cache/epub/13333/pg13333.txt | tail -9000 | head -7500 > $TMPFILE
-   curl http://www.gutenberg.org/cache/epub/13333/pg13333.txt > $TMPFILE
-fi
-
-# echo $TMPFILE;
-
-# print the title to our output text file
-# printf "\n\nDEMOCRACY IN AMERICA\n\n" > democracy_in_america.txt
-
-# this will shuffle our search terms
-# shuffle() {
-#    local i tmp size max rand
-
-#    # $RANDOM % (i+1) is biased because of the limited range of $RANDOM
-#    # Compensate by using a range which is a multiple of the array size.
-#    size=${#SEARCHES[*]}
-#    max=$(( 32768 / size * size ))
-
-#    for ((i=size-1; i>0; i--)); do
-#       while (( (rand=$RANDOM) >= max )); do :; done
-#       rand=$(( rand % (i+1) ))
-#       tmp=${SEARCHES[i]} SEARCHES[i]=${SEARCHES[rand]} SEARCHES[rand]=$tmp
-#    done
-# }
-
-# do the shuffle
-# shuffle
-
-# perform each string search and output to text file
-# for search in $SEARCH
-# do 
-	# run the same search for each word and do some simple string manipulation
-	cat < $TMPFILE | tail -9000 | head -7500 | grep -e $SEARCH | cut -d ' ' -f 2-5 | tr A-Z a-z | tr -d "',."
-   # OUT=$OUT`cat < $TMPFILE | tail -9000 | head -7500 | grep -e $SEARCH | cut -d ' ' -f 2-5 | tr A-Z a-z | tr -d "',."`
-	# printf "\n" >> $OUT
-   # OUT=$OUT"\n"
-# done
-
-# show the contents
-# cat democracy_in_america.txt
-# echo $OUT
+# run the search
+cat < $SRC | tail -9000 | head -7500 | grep -e $SEARCH | cut -d ' ' -f 2-5 | tr A-Z a-z | tr -d "',."
