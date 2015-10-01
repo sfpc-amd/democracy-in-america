828ca4a180dc80579305c9fa139cb35fe58ab8de
Andy Dayton <amdayton@gmail.com>
2015-10-01 07:30:20 -0400

    Fix script

--- bin/democracy_in_america.sh
+++ bin/democracy_in_america.sh
@@ -4,4 +4,4 @@ SRC="src/corpus.txt"
 read SEARCH
 
 # run the search
-cat < $SRC | tail -9000 | head -7500 | grep -e $SEARCH | cut -d ' ' -f 2-5 | tr A-Z a-z | tr -d "',."
+cat < $SRC | grep -e $SEARCH | cut -d ' ' -f 2-5 | tr A-Z a-z | tr -d "',."
