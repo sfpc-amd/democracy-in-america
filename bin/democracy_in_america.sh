9626db76b893814555e02ce1c1698e8ab47f1caa
Andy Dayton <amdayton@gmail.com>
2015-10-05 11:23:17 -0400

    Tweaked and renamed generator, added some more outputs

--- bin/democracy_in_america.sh
+++
@@ -1,7 +0,0 @@
-# DEMOCRACY IN AMERICA
-
-SRC="src/corpus.txt"
-read SEARCH
-
-# run the search
-cat < $SRC | grep -e $SEARCH | cut -d ' ' -f 2-5 | tr A-Z a-z | tr -d "',."
---
+++ bin/generate.sh
@@ -0,0 +1,7 @@
+# DEMOCRACY IN AMERICA
+
+SRC="src/corpus.txt"
+read SEARCH
+
+# run the search
+cat < $SRC | grep -e $SEARCH | cut -d ' ' -f 2-5 | tr A-Z a-z | tr -d "',._\""
