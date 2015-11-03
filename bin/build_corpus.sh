703be2b1b82edb409ecf6ab491a8d0764db485f4
Andy Dayton <amdayton@gmail.com>
2015-11-02 21:55:12 -0500

    Added wollstonecraft

--- bin/build_corpus.sh
+++ bin/build_corpus.sh
@@ -17,15 +17,20 @@ fi
 # 	cat src/democracy_in_america.2.txt | tail -14050 | head -13600 >> src/corpus.txt
 # fi
 
-read -p "Include 'Letters from of John Keats' (yn) " yn4
+read -p "Include 'Letters from of John Keats'? (yn) " yn4
 if echo "$yn4" | grep -iq "^y" ;then
 	cat src/letters_of_john_keats.txt | tail -14050 | head -13600 >> src/corpus.txt
 fi
 
-read -p "Include Stein' (yn) " yn5
+read -p "Include Stein? (yn) " yn5
 if echo "$yn5" | grep -iq "^y" ;then
 	cat src/gertrude_stein_carl_van_vechten.txt | tail -27300 | head -13600 >> src/corpus.txt
 fi
 
+
+read -p "Include Wollstonecraft'? (yn) " yn6
+if echo "$yn6" | grep -iq "^y" ;then
+	cat src/wollstonecraft.txt | tail -5145 | head -4785 >> src/corpus.txt
+fi
 
 printf "\nDone.\n\n"
