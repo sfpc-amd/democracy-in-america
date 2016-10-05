e8a76ef7b0a174070c5fed09e2758dd2f89d0fde
Andy Dayton <amdayton@gmail.com>
2016-10-05 00:38:57 -0400

    Added browning/barrett text and updated build corpus script

--- bin/build_corpus.sh
+++ bin/build_corpus.sh
@@ -7,16 +7,6 @@ if echo "$yn1" | grep -iq "^y" ;then
 	cat src/correspondence.txt | tail -9000 | head -7500 >> src/corpus.txt;
 fi
 
-# read -p "Include 'Democracy in America' Vol 1? (yn) " yn2
-# if echo "$yn2" | grep -iq "^y" ;then
-# 	cat src/democracy_in_america.1.txt | tail -19190 | head -18825 >> src/corpus.txt
-# fi
-
-# read -p "Include 'Democracy in America' Vol 2? (yn) " yn3
-# if echo "$yn3" | grep -iq "^y" ;then
-# 	cat src/democracy_in_america.2.txt | tail -14050 | head -13600 >> src/corpus.txt
-# fi
-
 read -p "Include 'Letters from of John Keats'? (yn) " yn4
 if echo "$yn4" | grep -iq "^y" ;then
 	cat src/letters_of_john_keats.txt | tail -14050 | head -13600 >> src/corpus.txt
@@ -28,9 +18,14 @@ if echo "$yn5" | grep -iq "^y" ;then
 fi
 
 
-read -p "Include Wollstonecraft'? (yn) " yn6
+read -p "Include Wollstonecraft? (yn) " yn6
 if echo "$yn6" | grep -iq "^y" ;then
 	cat src/wollstonecraft.txt | tail -5145 | head -4785 >> src/corpus.txt
+fi
+
+read -p "Include Browning/Barrett? (yn) " yn7
+if echo "$yn7" | grep -iq "^y" ;then
+	cat src/robert_browning_elizabeth_barrett.txt | tail -n 21105 | head -n 20728 >> src/corpus.txt
 fi
 
 printf "\nDone.\n\n"
